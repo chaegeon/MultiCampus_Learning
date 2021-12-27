ba4eca0eec11624ff6b16426fc756534d2e9de86
dico <chgeon.lim@gmail.com>
2021-12-27 22:31:47 +0900

    주석 추가,수정

--- workspace/dico/ikkuuu/urls.py
+++ workspace/dico/ikkuuu/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from django.urls.conf import include
 from . import views
+# .은 현재경로 -> 같은 경로의 views를 가져오라는 뜻
 
+#127,0.0.1:8000/board 까지 온 것.
+# 그 다음은 어디로 가지? 는 이 밑에서 설정
+# board 이후의 경로를 지정해주는 곳
 urlpatterns = [
     path('', views.index),
-
-    path( 'list/', views.list)
+    # '' 는 board까지 썼을 때 화면은 얘가 담당해준다는 뜻
+    # views의 index()를 호출
+    # path('list/', views.list ), #board를 쳤을 때 리스트가 나오게 하는 경우가 아니라,
+    # board/list를 쳐야 list가 나오게 할 경우
+    # 127.0.0.1:8000/board
+    
+    
+    path( 'list/', views.list) # => 127.0.0.1:8000/board/list``
     
 ]
--- workspace/dico/ikkuuu/views.py
+++ workspace/dico/ikkuuu/views.py
@@ -1,9 +1,13 @@
-
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-
+#def indes(request):
+#  return HttpResponse('board의 메인화면')
 def index( request):
+  #return HttpResponse('연결 완료')
+#def list( request ):
+  #return render(request, '연결이 완료되었습니다.')
+  # return 
   return render(request, 'ikkuuu/login.html')
 
 
