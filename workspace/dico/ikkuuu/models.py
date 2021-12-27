7005a311da633ab8dd7fdb65e9e482070709b03d
dico <chgeon.lim@gmail.com>
2021-12-27 22:27:43 +0900

    dico static 성공

--- workspace/dico/ikkuuu/models.py
+++ workspace/dico/ikkuuu/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Ikkuuu( models.Model):
+    creatDate = models.DateField()
+    writer = models.CharField(max_length=128)
+    subject = models.CharField(max_length=255)
+    content = models.TextField()
