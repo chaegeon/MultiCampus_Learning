bf36c4ab39759e59b86919386e7fa557fad73150
dico <chgeon.lim@gmail.com>
2021-12-30 17:09:12 +0900

    modify.py 에러 수정

--- workspace/simpleBoard/board/models.py
+++ workspace/simpleBoard/board/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 # ORM(Object Realation Mapping)
 class Board( models.Model): # models모듈 내의 Model클래스를 상속받아 새로운 클래스 Board를 정의
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # on_delete~ : 유저가 삭제되면 그 유저에 해당하는 보드도 삭제하게 하는 설정
     createDate = models.DateField()
-    writer = models.CharField(max_length=128)
+    # writer = models.CharField(max_length=128) # 결국 Username이라 없어도 되긴 한데..,,
     subject = models.CharField(max_length=255)
     content = models.TextField()
-    # 4개의 컬럼 지정
--- workspace/simpleBoard/board/views.py
+++ workspace/simpleBoard/board/views.py
@@ -7,7 +7,6 @@ from .models import Board
 # .은 현재 패키지라는 뜻 
 # 현재디렉토리의 모델스라는 모듈의 Board를 import
 
-
 # Create your views here.
 #def indes(request):
 #  return HttpResponse('board의 메인화면')
@@ -25,13 +24,11 @@ def index( request):
   #return render(request, '연결이 완료되었습니다.')
   # return 
   
-
 #def index( request ):
   # DB의 내용을 브라우저에 전달하기 위한 코드를 추가
   #todos = Board.objects.all() # 테이블 내의 모든 내용을 조회
   #content = {'':}
   #return render( request, 'board/list.html', content) 
-
 
 def login(request):
   return render(request, 'board/login.html')
@@ -39,41 +36,51 @@ def login(request):
 def write(request):
   return render(request, 'board/write.html')
 
+#데코레이터를 이용해서 로그인이 필요한 함수
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='/accounts/signIn/')
 def create(request):
   #데이터를 전달받는다
   #print(request.POST['createDate'])
   #print(request.POST['user'])
   #print(request.POST['subject'])
   #print(request.POST['content'])
-  # 이렇게 print해놓고 브라우저에서 글작성하면 터미널에도 보임
-  # 이걸 DB에 넣을 거임
+  # 이렇게 print해놓고 브라우저에서 글작성하면 터미널에도 보임 이걸 DB에 넣을 거
+  
+  if request.method == 'POST':
+    new = Board(
+      user        = request.user,
+      createDate  = request.POST['createDate'],
+      subject     = request.POST['subject'],
+      content     = request.POST['content'],
+      )
+    new.save()
+    return HttpResponseRedirect( reverse ('list'))
+    # 글작성 하고 작성완료 뜬 후 보드 페이지로 돌아가도록
+  else :
+    # 로그인이 되어 있지 않은 경우 로그인 이후에 새로 글을 작성해주자..,,
+    return render(request, 'board/write.html')
 
-  new = Board(
-    createDate = request.POST['createDate'],
-    writer = request.POST['user'],
-    subject = request.POST['subject'],
-    content = request.POST['content'],
-    )
-  new.save()
 
-  return HttpResponseRedirect(reverse('list'))
-    # 글작성 하고 작성완료 뜬 후 보드 페이지로 돌아가게
-
+@login_required(login_url='accounts/signIn/')
 def delete( request ):
   #print(request.POST['id'])
   b = Board.objects.get(id= request.POST['id'])
   b.delete()
-  return HttpResponseRedirect(reverse('list')) 
+  return HttpResponseRedirect(reverse('list'))
 
+@login_required(login_url='accounts/signIn/')
 def update( request):
   #print('id', request.GET['id']) #list에서 id를 보내면 여기서 id를 받아옴
   
-  #수정하려면 기존 내용을 가져와야 수정하지
+  #수정하려면 기존 내용을 가져와야 수정가능하기 때문에
   post = Board.objects.get(id = request.GET['id'])
-  content = {'post': post} # 가져와서 콘텐트에 넣어
+  content = {'post': post} # 가져와서 content에 넣어
   return render( request, 'board/update.html', content)
 
-def modify(request): # write? create와의 차이점은 객체를 만들지 않아. 기존의 객체를 가져옴
+@login_required(login_url='accounts/signIn/')
+def modify(request): # write? create와의 차이점은 객체를 만들지 않음. 기존의 객체를 가져옴
   post = Board.objects.get(id=request.POST['id'])
   post.createDate = request.POST['createDate']
   post.writer = request.POST['user']
