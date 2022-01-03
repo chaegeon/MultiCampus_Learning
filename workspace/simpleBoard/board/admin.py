c977d62878405bbb24b4eb9cbda7adff039bf568
dico <chgeon.lim@gmail.com>
2022-01-03 09:38:41 +0900

    관리자 페이지 admin 설정

--- workspace/simpleBoard/board/admin.py
+++ workspace/simpleBoard/board/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Board
+
 
 # Register your models here.
+admin.site.register(Board)
+# import 하고 이렇게 등록만 시켜주면
+# 관리자 페이지(admin)에 Board를 관리할 수 있는 탭이 생김
