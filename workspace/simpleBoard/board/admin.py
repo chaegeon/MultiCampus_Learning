de8e2c10f384ca20a56f46350883ef97aff95a45
dico <chgeon.lim@gmail.com>
2022-01-03 10:13:22 +0900

    장고 마무리

--- workspace/simpleBoard/board/admin.py
+++ workspace/simpleBoard/board/admin.py
@@ -13,6 +13,6 @@ class BoardAdmin( admin.ModelAdmin):
     # 지금은 날짜를 클릭해야 해당 글로 들어가지는데
     # 제목을 클릭해서 해당 글의 상세페이지로 이동할 수 있도록
 
-admin.site.register(Board, BoardAdmin)
+admin.site.register(Board, BoardAdmin) 
 # import 하고 이렇게 등록만 시켜주면
 # 관리자 페이지(admin)에 Board를 관리할 수 있는 탭이 생김
