044eb22d678fe591bfe5bc40a81f35ce126aeb69
dico <chgeon.lim@gmail.com>
2022-01-18 20:47:44 +0900

    oecd 완료

--- 프로젝트_01/01_.py
+++ 프로젝트_01/01_.py
@@ -1,4 +1,4 @@
-# !pip install xmltodict #
+### !pip install xmltodict
 
 import pandas as pd
 import numpy as np
@@ -11,8 +11,8 @@ drive.mount('/content/drive')
 oecd = pd.read_csv('/content/drive/MyDrive/멀티캠퍼스/실습/Data폴더/oecd_killed.csv')
 oecd
 # 2020년 자료/ oecd 65세 이상 보행자 교통사고 사망자
-# 그래프에 //단위: 명/10만명// 추가할 것
-# 가능하면 oecd국들의 평균도 추가할 것
+# 그래프에 //단위: 명/10만명// 이라는 설명을 추가할 것
+# oecd국들의 평균도 추가해볼 것
 
 # 출처: https://stats.oecd.org/
 
@@ -73,6 +73,7 @@ print(average)
 
 average
 
+# 평균값을 구한 average 평균선 추가해보기
 plt.figure(figsize=(20,5))
 n_data = len(Country)
 index = np.arange(len(Country))
@@ -82,12 +83,14 @@ plt.legend()
 plt.ylabel('사망자 비율')
 plt.grid(True, alpha = 0.5, axis = 'y')
 
-plt.axhline( y = average, color='r', linewidth = 1, label='평균') # 평균값을 구한 average 평균선 추가하기
+plt.axhline( y = average, color='r', linewidth = 1, label='평균') 
 plt.title('OECD국가별 10만명당 노인보행 중 사망자 수')
 plt.show() 
 
-# 텍스트를 추가항 설명 보충
-plt.figure(figsize=(20,5))
+# average값으로 평균선 추가하기
+# 그래프 내의 여백 정리
+# 그래프 내에 텍스트를 추가하여 설명 보충
+plt.figure(figsize=(23,5))
 n_data = len(Country)
 index = np.arange(len(Country))
 plt.bar(Country,Per)
@@ -95,20 +98,51 @@ plt.bar(Country,Per)
 plt.legend()
 plt.ylabel('사망자 비율')
 plt.grid(True, alpha = 0.5, axis = 'y')
+plt.margins(x=0.01)
 
-plt.axhline( y = average, color='r', linewidth = 1) # 평균값을 구한 average 평균선 추가하기
+plt.axhline( y = average, color='r', linewidth = 1) 
 plt.title('OECD국가별 10만명당 노인보행 중 사망자 수')
 
-plt.text(20.3 ,6.5,'OECD 평균: 6.2명', color='r', verticalalignment='bottom' , horizontalalignment='center' )
-plt.text(20.3 ,16.5,'단위: 명/10만 명', verticalalignment='bottom' , horizontalalignment='center' )
+plt.text(19.6, 6.5,'OECD 평균: 6.2명', color='r', verticalalignment='bottom' , horizontalalignment='center' )
+
+plt.show() 
+
+# average값으로 평균선 추가하기
+# 값을 소수점 첫째자리까지 표시
+# 그래프 내에 텍스트를 추가하여 설명 보충
+plt.figure(figsize=(23,5))
+n_data = len(Country)
+index = np.arange(len(Country))
+plt.bar(Country,Per)
+
+plt.legend()
+plt.ylabel('사망자 비율')
+plt.grid(True, alpha = 0.5, axis = 'y')
+plt.margins(x=0.01)
+
+plt.axhline( y = average, color='r', linewidth = 1) 
+plt.title('OECD국가별 10만명당 노인보행 중 사망자 수')
+
+for i in range( len(Country)):
+  plt.text( i, Per[i]/2, '%.1f'% Per[i], ha = 'center', va='bottom', color = 'w', size=15)
+
+plt.text(19.6, 6.5,'OECD 평균: 6.2명', color='r', verticalalignment='bottom' , horizontalalignment='center' )
+plt.text(19.7, 16.3,'노인: 65세 이상', verticalalignment='bottom' , horizontalalignment='center' )
+plt.text(19.74, 15.1,'단위: 명/10만 명', verticalalignment='bottom' , horizontalalignment='center' )
 
 plt.show() 
 
 
 
-# 테스트
 
 
+
+
+
+
+# 연습
+
+s
 
 plt.figure(figsize=(20,5))
 n_data = len(Country)
@@ -142,6 +176,8 @@ plt.show()
 oecd2 = oecd.sort_values('Per',ascending=True)
 oecd2
 
+
+
 Country2 = oecd2['Country'].values.tolist()
 Country2
 
@@ -167,6 +203,12 @@ ax2.bar(Counrty, average, color='deeppink', label='Demand', alpha=0.7, width=0.7
 ax2.set_ylim(0, 18)
 ax2.set_ylabel(r'Demand ($\times10^6$)')
 ax2.tick_params(axis='y', direction='in')
+
+
+
+
+
+
 
 fig = plt.figure(figsize=(20,5))
 ax = fig.add_subplot()
