123b95e4d2b8c1a444f5f4daa56fc65fa9e1f11d
dico <chgeon.lim@gmail.com>
2022-01-20 11:47:59 +0900

    tip 수정

--- 프로젝트_01/그래프그리기.py
+++ 프로젝트_01/그래프그리기.py
@@ -5,14 +5,19 @@ num_icecream = [1000, 2000, 1100, 900, 1500, 1550, 2200]
 
 fig = plt.figure(figsize=(8,8)) ## Figure 생성 
 fig.set_facecolor('white') ## Figure 배경색 지정
+ax1 = fig.add_subplot() ## axes 생성
  
 colors = sns.color_palette('summer', len(day)) ## 바 차트 색상
  
 xtick_label_position = list(range(len(day))) ## x축 눈금 라벨이 표시될 x좌표
-plt.xticks(xtick_label_position, day) ## x축 눈금 라벨 출력
+ax1.set_xticks(xtick_label_position) ## x축 눈금 
+ax1.set_xticklabels(day) ## x축 눈금 라벨
+ax1.bar(xtick_label_position, num_icecream, color=colors) ## 바차트 출력
  
-plt.bar(xtick_label_position, num_icecream, color=colors) ## 바차트 출력
-plt.plot(xtick_label_position, num_icecream, color='b',
-         linestyle='--', marker='o') ## 선 그래프 출력
-plt.title('Sales Icecream for Days', fontsize=20)
+color = 'blue'
+ax2 = ax1.twinx() ## 새로운 axis 생성
+ax2.plot(xtick_label_position, temp, color=color, linestyle='--', marker='o') ## 선 그래프 
+ax2.tick_params(axis='y', labelcolor=color) ## 눈금 라벨 색상 지정
+ 
+plt.title('Sales Icecream and Temperature for Days', fontsize=20)
 plt.show()
