b4f11f47670f243b6e5e80b6be8ace4937955ae8
dico <chgeon.lim@gmail.com>
2022-01-21 14:59:43 +0900

    사고 발생 상황유형 ( 보행자 행동별) 작업중

--- 프로젝트_01/03_사고발생_상황유형_보행자행동.py
+++ 프로젝트_01/03_사고발생_상황유형_보행자행동.py
@@ -28,3 +28,41 @@ action
 
 action = pd.merge(action, etc, left_index = True, right_index=True)
 action
+
+action.columns
+
+action.loc[0]
+
+action = action.drop(columns=['0_x', '0_y', '0'])
+action
+
+action.columns = ['횡단중', '차도통행중', '보도통행중', '길가장자리통행중', '기타']
+
+action.index = ['사고발생건수', '사망자수', '부상자수']
+action
+
+#action
+
+action_a = action.columns
+
+action = action.T
+action
+
+action = action.reset_index()
+action
+
+action.columns=['행동유형', '사고발생건수', '사망자수', '부상자수']
+action
+
+a_type = action['행동유형'].values.tolist()
+
+a_type
+
+num = action['사고발생건수'].values.tolist()
+num
+
+death = action['사망자수'].values.tolist()
+death
+
+injury = action['부상자수'].values.tolist()
+injury
