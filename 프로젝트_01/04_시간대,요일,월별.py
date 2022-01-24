52d62c07bfa0d1492ed2943341add8e6d6a50d6e
dico <chgeon.lim@gmail.com>
2022-01-24 15:26:19 +0900

    요일별 교통사고 현황

--- 프로젝트_01/04_시간대,요일,월별.py
+++ 프로젝트_01/04_시간대,요일,월별.py
@@ -93,3 +93,92 @@ plt.show()
 - 의외로 그 다음은 오전 8시~10시
 - 오후 8시~10시부터 절반수준으로 내려감 
 - 어두운 시간보다는 밝은 시간에 사고가 더 많이 일어남
+
+
+# 요일별
+
+rawData = pd.read_csv('/content/drive/MyDrive/멀티캠퍼스/실습/Data폴더/day_a.csv')
+rawData
+
+# 출처: TAAS 교통사고분석시스템(http://taas.koroad.or.kr/)
+
+rawData = rawData.T
+rawData
+
+rawData.index
+
+rawData .columns
+
+seoul = rawData.loc[['0', '1']]
+
+seoul
+
+seoul = seoul.drop(columns = [0])
+
+seoul = seoul.drop(columns = [1])
+
+seoul = seoul.T
+seoul
+
+seoul.index = ['0','1','2','3','4','5','6']
+
+seoul
+
+seoul = seoul.astype({'1':int})
+
+seoul.info()
+
+day = seoul['0'].values.tolist()
+day
+
+accident = seoul['1'].values.tolist()
+accident
+
+seoul
+
+## 요일별 시각화
+
+
+n_data = len(day)
+index = np.arange(len(day))
+plt.bar(day, accident)
+plt.show()
+
+n_data = len(day)
+index = np.arange(len(day))
+plt.bar(day, accident)
+plt.grid(True, alpha = 0.5, axis = 'y')
+
+for i in range( len(day)):
+  plt.text( i-0.22, accident[i]+4, f'{accident[i]}', size =13)
+
+plt.show()
+
+# 내림차순으로 정렬
+seoul_sort = seoul.sort_values('1',ascending=False)
+seoul_sort
+
+day_s = seoul_sort['0'].values.tolist()
+day_s
+
+accident_s = seoul_sort['1'].values.tolist()
+accident_s
+
+## 결론
+
+plt.figure(figsize=(15,5))
+n_data = len(day_s)
+index = np.arange(len(day_s))
+plt.bar(day_s, accident_s)
+plt.grid(True, alpha = 0.5, axis = 'y')
+plt.ylim([0, 375])
+plt.legend()
+plt.title('월별 사고발생건수')
+
+for i in range( len(day_s)):
+  plt.text( i-0.1, accident_s[i]+3, f'{accident_s[i]}', size =14)
+
+
+plt.show()
+
+- 별 상관 없는 것 같다..
