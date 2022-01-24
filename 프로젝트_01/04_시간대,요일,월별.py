6c453e5e4e73b6d9a178dbe106856faf9f426cbd
dico <chgeon.lim@gmail.com>
2022-01-24 15:27:10 +0900

    월별 교통사고 현황

--- 프로젝트_01/04_시간대,요일,월별.py
+++ 프로젝트_01/04_시간대,요일,월별.py
@@ -182,3 +182,103 @@ for i in range( len(day_s)):
 plt.show()
 
 - 별 상관 없는 것 같다..
+
+
+
+# 월별
+
+rawData = pd.read_csv('/content/drive/MyDrive/멀티캠퍼스/실습/Data폴더/month_a.csv')
+rawData
+
+# 출처: TAAS 교통사고분석시스템(http://taas.koroad.or.kr/)
+
+rawData = rawData.T
+rawData
+
+rawData.index
+
+rawData .columns
+
+seoul = rawData.loc[['0', '1']]
+
+seoul
+
+seoul = seoul.drop(columns = [0])
+
+seoul = seoul.drop(columns = [1])
+
+seoul = seoul.T
+seoul
+
+seoul.index = ['0','1','2','3','4','5','6','7','8','9','10','11','12']
+
+seoul
+
+seoul = seoul.astype({'1':int})
+
+seoul.info()
+
+seoul = seoul.drop(index=['0'])
+
+seoul
+
+month = seoul['0'].values.tolist()
+month
+
+accident = seoul['1'].values.tolist()
+accident
+
+seoul
+
+## 월별 시각화
+
+plt.figure(figsize=(20,5))
+n_data = len(month)
+index = np.arange(len(month))
+plt.bar(month, accident)
+plt.show()
+
+plt.figure(figsize=(23,5))
+n_data = len(month)
+index = np.arange(len(month))
+plt.bar(month, accident)
+plt.grid(True, alpha = 0.5, axis = 'y')
+
+for i in range( len(month)):
+  plt.text( i-0.1, accident[i]+2, f'{accident[i]}', size =15)
+
+plt.show()
+
+# 내림차순으로 정렬
+seoul_sort = seoul.sort_values('1',ascending=False)
+seoul_sort
+
+month_s = seoul_sort['0'].values.tolist()
+month_s
+
+accident_s = seoul_sort['1'].values.tolist()
+accident_s
+
+## 결론
+
+plt.figure(figsize=(23,5))
+n_data = len(month_s)
+index = np.arange(len(month_s))
+plt.bar(month_s, accident_s)
+plt.grid(True, alpha = 0.5, axis = 'y')
+plt.ylim([0, 225])
+plt.legend()
+plt.title('월별 사고발생건수')
+
+for i in range( len(month_s)):
+  plt.text( i-0.1, accident_s[i]+2.3, f'{accident_s[i]}', size =14)
+
+
+plt.rc('xtick', labelsize=20)   
+plt.rc('ytick', labelsize=20)  
+
+
+plt.show()
+
+- 1, 10, 11월은 가장 많이 사고가 일어나지만 12월은 적고
+- 다른 월과의 차이도 크지 않은 걸 보면 겨울과는 상관없나..
