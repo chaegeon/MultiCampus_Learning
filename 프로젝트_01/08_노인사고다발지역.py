89f3727261b0a77088993c0f548387534903b474
dico <chgeon.lim@gmail.com>
2022-02-24 09:53:12 +0900

    보행노인사고다발지역

--- 프로젝트_01/08_노인사고다발지역.py
+++ 프로젝트_01/08_노인사고다발지역.py
@@ -126,3 +126,54 @@ for i in oldman.index:
 map = folium.Map( location = [37.53, 127], zoom_start=12, tiles="cartodbpositron" )
 MarkerCluster( location, overlay=True).add_to(map)
 map
+
+### 사고 발생건수+사상자수
+- 사고 발생건수와 사상자수의 합 상위 13개 지점을 로드뷰를 통하여 분석하였으며 주로 다음과 같은 특징이 존재
+ - 교차로
+ - 시장(전통시장, 도매시장, 상가)
+ - 신호등이 없는 횡단보도(무신호 횡단로)
+ - 병원
+ - 기타(주택가에 위치, 고가도로)
+
+- 이러한 특징들을 변수로 하여 보행노인사고와의 인과관계를 분석해 볼 수 있지 않을까?
+- (사고발생지점의 특징이 주어졌을 때 노인보행자 교통사고가 발생할 확률을 구해보는 것도 재미있을 것 같다. 근데 할 수 있을진 모르겠음)
+
+### 사고다발지역+노인보호구역
+
+silverzone = pd.read_csv('/content/drive/MyDrive/[D9&10] 데이터시각화 프로젝트_5조/Data/silverzone.csv')
+silverzone.head()
+
+len(silverzone)
+
+from folium import Circle
+
+m = folium.Map(location=[37.58, 127.0], tiles="cartodbpositron", zoom_start=11)
+
+# 지도에 사고다발지점 표시(빨간색 원, 반경 200미터)
+for i in oldman.index :
+    folium.Circle(
+        location = oldman.loc[i, ['위도', '경도']],
+        radius = 200,
+        color = 'red'
+    ).add_to(m)
+
+# 지도에 노인보호구역 표시(파란색 원, 반경 300미터)
+for i in silverzone.index :
+    folium.Circle(
+        location = silverzone.loc[i, ['위도', '경도']],
+        radius = 300,
+        color = 'dodgerblue'
+    ).add_to(m)
+
+
+m
+
+- 서울시 전체 노인보호구역 145곳 중 17곳이 사고다발지역을 포함
+ - 사고다발지역에 추가로 노인보호구역을 지정할 필요가 있음 
+- 노인보호구역으로 지정되었음에도 불구하고 사고다발지역인 지점 존재
+ - 노인보호구역에 대한 인식 재고 및 사후관리 필요
+
+ ### 사고다발지역 + 고령인구비율
+
+population = pd.read_csv('/content/drive/MyDrive/[D9&10] 데이터시각화 프로젝트_5조/Data/2020_고령인구비율.csv', encoding='cp949')
+population
